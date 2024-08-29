157e547decd22fac55e6fc46668b5085bf19f7f5
majprofe <manuelarroyojuan@iesjandula.es>
2024-08-29 14:25:33 +0200

    Added caroulsel

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,22 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: [
-		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
-		'./src/styles/**/*.{css,scss}', // Incluye la carpeta de estilos
+	  './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+	  './src/styles/**/*.{css,scss}',
 	],
 	theme: {
-		fontFamily: {
-			sans: ['Gotham SSm A', 'sans-serif'],
+	  fontFamily: {
+		sans: ['Gotham SSm A', 'sans-serif'],
+	  },
+	  extend: {
+		colors: {
+		  primary: '#000000',
+		  secondary: '#FFFFFF',
+		  tertiary: '#fde047',
 		},
-		extend: {
-			colors: {
-				primary: '#000000',
-				secondary: '#FFFFFF',
-				tertiary: '##DC2626',
-			},
-		},
+	  },
 	},
 	plugins: [
-		require('@midudev/tailwind-animations'),
+	  require('@midudev/tailwind-animations'),
 	],
-}
+  }
+  
